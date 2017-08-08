5c914f66d2111da893bf8008b5b484449a7b8ebf
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-08-08 16:49:08 -0400

    Added the splash form

---
+++ COMP123-S2017-LESSON12B2/SplashForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/* Name: Sylvere Ekponon
+ * Date: August 8, 2017
+ * Description: Calculator Demo Project
+ * Version: 0.1 - Added the SplashForm
+ */
+namespace COMP123_S2017_LESSON12B2
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
